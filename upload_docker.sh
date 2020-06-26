97db113baf3d6e10319d744bc0e9cb5820e9cb47
Abdullah ALShayie <36479470+abdullah2web@users.noreply.github.com>
2020-06-26 05:38:41 +0300

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,15 +1,18 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=abdullah2web/devops
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=abdullah2web
 
 # Step 3:
 # Push image to a docker repository
+docker tag ${dockerpath} ${dockerpath}
+docker push ${dockerpath}
