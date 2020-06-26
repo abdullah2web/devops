b33101dec061091de20374a73a506f732dbe0010
Abdullah ALShayie <36479470+abdullah2web@users.noreply.github.com>
2020-06-26 05:34:25 +0300

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/env bash
-
+!/home/ubuntu/.devops/bin/en
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=demolocal .
+docker build --tag=abdullah2web/devops .
 
 # Step 2: 
 # List docker images
@@ -12,4 +11,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -it demolocal bash
+docker run -p 8000:80 abdullah2web/devops 
